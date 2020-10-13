df7367934cf9ba51a610270d8d928c1a5cc214f0
Hero74 <xodnwong9596@gmail.com>
2020-10-13 19:30:29 +0900

    손상 복원 기능 수정

--- ColorRecovery/demege_recovery.py
+++ ColorRecovery/demege_recovery.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('profile_damage.jpg')
-mask = cv2.imread('mask.png',0)
+img = cv2.imread('profile_damage2.jpg')
+mask = cv2.imread('mask2.png',0)
 
 damage_recovery = cv2.inpaint(img,mask,3,cv2.INPAINT_TELEA)
 
