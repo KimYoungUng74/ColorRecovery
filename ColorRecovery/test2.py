4c1ea8d7d5b19c3fa24ad5a08889f1c569bd2713
Hero74 <xodnwong9596@gmail.com>
2020-10-13 19:21:13 +0900

    사진 손상 복원 구현

--- ColorRecovery/test2.py
+++ ColorRecovery/test2.py
@@ -1,3 +1,12 @@
-import torch
+import numpy as np
+import cv2
 
-torch.cuda.get_device_name(0)
+img = cv2.imread('profile_damage.jpg')
+mask = cv2.imread('mask.png',0)
+
+damage_recovery = cv2.inpaint(img,mask,3,cv2.INPAINT_TELEA)
+
+cv2.imshow('damage_recovery_before',img)
+cv2.imshow('damage_recovery_after',damage_recovery)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
